59110e7e0f26310155b9e4976d1d84dc8ce079f5
LiamLiu <liamliu@boyaa.com>
2022-07-05 18:35:17 +0800

    corutine pool

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pool
-Coroutine Pool
+# Pool
+Coroutine Pool  
+# Quick start
+```
+    p := NewPool(4)  
+    go p.Run()  
+    go http.ListenAndServe("0.0.0.0:6060", nil)  //pprof查看协程个数
+    for {
+        p.Put(Info{Param: "http://www.boyaa.com", Func: Proc})
+        time.Sleep(time.Millisecond * 500)
+    }
+```
+
---
+++ pool.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+type Info struct {
+	Param string
+	Func  func(string)
+}
+
+type Task struct {
+	info chan Info
+}
+
+type Pool struct {
+	tasks *Task
+	num   int
+}
+
+func NewPool(n int) *Pool {
+	taskChan := make(chan Info)
+	return &Pool{
+		tasks: &Task{
+			info: taskChan,
+		},
+		num: n,
+	}
+}
+
+func (p *Pool) Put(info Info) {
+	p.tasks.info <- info
+}
+
+func (p *Pool) Run() {
+	for gc := 0; gc < p.num; gc++ {
+		go p.Work(gc)
+	}
+}
+
+func (p *Pool) Work(workId int) {
+	for {
+		select {
+		case job := <-p.tasks.info:
+			params := fmt.Sprintf("url:%s workid:%d", job.Param, workId)
+			job.Func(params) //执行传入的函数
+		}
+	}
+}
+
+func Proc(str string) {
+	fmt.Println("Result:", str)
+}
+
+func main() {
+
+	p := NewPool(4)
+	go p.Run()
+	go http.ListenAndServe("0.0.0.0:6060", nil)
+	for {
+		p.Put(Info{Param: "http://www.boyaa.com", Func: Proc})
+		time.Sleep(time.Millisecond * 500)
+	}
+}
